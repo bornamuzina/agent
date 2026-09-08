d31b9f582de44e060657cdd701ecbdb5b15f61b4
bornamuzina <borna.muzinam@gmail.com>
2026-09-08 18:18:36 +0200

    Add CDP browser control

---
+++ browser.py
@@ -0,0 +1,37 @@
+import pychrome
+
+CHROME_URL = "http://localhost:9222"
+
+def connect():
+    browser = pychrome.Browser(url=CHROME_URL)
+    tab = browser.new_tab()
+    tab.start()
+    return tab
+
+def goto(tab, url):
+    tab.Page.enable()
+    tab.Page.navigate(url=url)
+    tab.wait(3)
+
+def read_text(tab):
+    reply = tab.Runtime.evaluate(expression="document.body.innerText")
+    return reply["result"]["value"]
+
+def list_links(tab):
+    js = """
+    Array.from(document.querySelectorAll('a'))
+      .filter(a => a.innerText.trim())
+      .slice(0, 30)
+      .map((a, i) => i + ': ' + a.innerText.trim() + ' -> ' + a.href)
+      .join('\\n')
+    """
+    reply = tab.Runtime.evaluate(expression=js)
+    return reply["result"]["value"]
+
+def click_link(tab, index):
+    js = f"""
+    Array.from(document.querySelectorAll('a'))
+      .filter(a => a.innerText.trim())[{index}].click()
+    """
+    tab.Runtime.evaluate(expression=js)
+    tab.wait(3)
